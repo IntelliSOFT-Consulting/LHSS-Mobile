672b1588c00176edf83f4efe63e3299fb8cfe4d8
davidnjau <davidnjau21@gmail.com>
2023-11-23 12:15:48 +0300

    initial commit

---
+++ app/src/main/java/com/intellisoft/lhss/shared/Login.kt
@@ -0,0 +1,12 @@
+package com.intellisoft.lhss.shared
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.intellisoft.lhss.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
