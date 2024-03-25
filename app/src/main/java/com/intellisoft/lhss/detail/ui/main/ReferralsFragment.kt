feb25a25cc1f9d9082c32dd671d376af51df03f0
davidnjau <davidnjau21@gmail.com>
2024-03-25 09:57:37 +0300

    Bug fixed on the referral

--- app/src/main/java/com/intellisoft/lhss/detail/ui/main/ReferralsFragment.kt
+++ app/src/main/java/com/intellisoft/lhss/detail/ui/main/ReferralsFragment.kt
@@ -3,6 +3,7 @@ package com.intellisoft.lhss.detail.ui.main
 import android.app.Application
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -101,10 +102,15 @@ class ReferralsFragment : Fragment() {
     private fun getReferrals() {
 
         CoroutineScope(Dispatchers.IO).launch {
+            var destinationFacility = ""
             val practitionerFacility = formatterClass.getSharedPref("practitionerFacility", requireContext())
+            if (practitionerFacility != null){
+                destinationFacility = practitionerFacility.replace("-", " ")
+            }
 
             val encounterList = patientDetailsViewModel.getWorkflowData("REFERRALS")
-            val listValue = encounterList.filterNotNull().filter { it.destination == practitionerFacility }
+
+            val listValue = encounterList.filterNotNull().filter { it.destination == destinationFacility }
 
             val visitHistoryAdapter = VisitHistoryAdapter(ArrayList(listValue), requireContext())
             CoroutineScope(Dispatchers.Main).launch {
