482fcb4db0f1f19b9112e18db5d560911248b218
Dave <davidnjau21@gmail.com>
2024-11-26 22:20:27 +0300

    Added color variants for the app bar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,32 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
+    id("androidx.navigation.safeargs.kotlin")
+//    id("com.google.firebase.crashlytics")
+//    id("com.google.gms.google-services")
+
 }
 
 android {
     namespace = "com.intellisoft.lhss"
-    compileSdk = 33
-
+    compileSdk = 34
     defaultConfig {
         applicationId = "com.intellisoft.lhss"
-        minSdk = 26
+        minSdk = 24
+        targetSdk = 34
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appAuthRedirectScheme"] = applicationId!!
         buildFeatures.buildConfig = true
+        versionCode = 55
+        versionName = "Live"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     buildFeatures { viewBinding = true }
@@ -44,111 +52,49 @@ android {
     }
 }
 
-configurations.all {
-    resolutionStrategy {
-        force(HapiFhir.caffeine)
-
-        force(HapiFhir.fhirBase)
-        force(HapiFhir.fhirClient)
-        force(HapiFhir.fhirCoreConvertors)
-
-        force(HapiFhir.fhirCoreDstu2)
-        force(HapiFhir.fhirCoreDstu2016)
-        force(HapiFhir.fhirCoreDstu3)
-        force(HapiFhir.fhirCoreR4)
-        force(HapiFhir.fhirCoreR4b)
-        force(HapiFhir.fhirCoreR5)
-        force(HapiFhir.fhirCoreUtils)
-
-        force(HapiFhir.structuresDstu2)
-        force(HapiFhir.structuresDstu3)
-        force(HapiFhir.structuresR4)
-        force(HapiFhir.structuresR5)
-
-        force(HapiFhir.validation)
-        force(HapiFhir.validationDstu3)
-        force(HapiFhir.validationR4)
-        force(HapiFhir.validationR5)
-    }
-}
-
-object Versions {
-    const val caffeine = "2.9.1"
-
-    // Hapi FHIR and HL7 Core Components are interlinked.
-    // Newer versions of HapiFhir don't work on Android due to the use of Caffeine 3+
-    // Wait for this to release (6.3): https://github.com/hapifhir/hapi-fhir/pull/4196
-    const val hapiFhir = "6.0.1"
-
-    // Newer versions don't work on Android due to Apache Commons Codec:
-    // Wait for this fix: https://github.com/hapifhir/org.hl7.fhir.core/issues/1046
-    const val hapiFhirCore = "5.6.36"
-}
-
-object HapiFhir {
-    const val fhirBase = "ca.uhn.hapi.fhir:hapi-fhir-base:${Versions.hapiFhir}"
-    const val fhirClient = "ca.uhn.hapi.fhir:hapi-fhir-client:${Versions.hapiFhir}"
-    const val structuresDstu2 = "ca.uhn.hapi.fhir:hapi-fhir-structures-dstu2:${Versions.hapiFhir}"
-    const val structuresDstu3 = "ca.uhn.hapi.fhir:hapi-fhir-structures-dstu3:${Versions.hapiFhir}"
-    const val structuresR4 = "ca.uhn.hapi.fhir:hapi-fhir-structures-r4:${Versions.hapiFhir}"
-    const val structuresR4b = "ca.uhn.hapi.fhir:hapi-fhir-structures-r4b:${Versions.hapiFhir}"
-    const val structuresR5 = "ca.uhn.hapi.fhir:hapi-fhir-structures-r5:${Versions.hapiFhir}"
-
-    const val validation = "ca.uhn.hapi.fhir:hapi-fhir-validation:${Versions.hapiFhir}"
-    const val validationDstu3 =
-        "ca.uhn.hapi.fhir:hapi-fhir-validation-resources-dstu3:${Versions.hapiFhir}"
-    const val validationR4 = "ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:${Versions.hapiFhir}"
-    const val validationR5 = "ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r5:${Versions.hapiFhir}"
-
-    const val fhirCoreDstu2 = "ca.uhn.hapi.fhir:org.hl7.fhir.dstu2:${Versions.hapiFhirCore}"
-    const val fhirCoreDstu2016 = "ca.uhn.hapi.fhir:org.hl7.fhir.dstu2016may:${Versions.hapiFhirCore}"
-    const val fhirCoreDstu3 = "ca.uhn.hapi.fhir:org.hl7.fhir.dstu3:${Versions.hapiFhirCore}"
-    const val fhirCoreR4 = "ca.uhn.hapi.fhir:org.hl7.fhir.r4:${Versions.hapiFhirCore}"
-    const val fhirCoreR4b = "ca.uhn.hapi.fhir:org.hl7.fhir.r4b:${Versions.hapiFhirCore}"
-    const val fhirCoreR5 = "ca.uhn.hapi.fhir:org.hl7.fhir.r5:${Versions.hapiFhirCore}"
-    const val fhirCoreUtils = "ca.uhn.hapi.fhir:org.hl7.fhir.utilities:${Versions.hapiFhirCore}"
-    const val fhirCoreConvertors = "ca.uhn.hapi.fhir:org.hl7.fhir.convertors:${Versions.hapiFhirCore}"
-
-    // Runtime dependency that is required to run FhirPath (also requires minSDK of 26).
-    // Version 3.0 uses java.lang.System.Logger, which is not available on Android
-    // Replace for Guava when this PR gets merged: https://github.com/hapifhir/hapi-fhir/pull/3977
-    const val caffeine = "com.github.ben-manes.caffeine:caffeine:${Versions.caffeine}"
-}
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
-    implementation("androidx.activity:activity-ktx:1.7.2")
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.2")
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.0")
-    implementation("androidx.work:work-runtime-ktx:2.8.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.8.22")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.6.0")
-    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("androidx.fragment:fragment-ktx:1.8.3")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.20")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.5")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.5")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.8.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.8.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
+    implementation("com.google.android.material:material:1.12.0")
     implementation("com.jakewharton.timber:timber:5.0.1")
     implementation("net.openid:appauth:0.11.1")
     implementation("com.auth0.android:jwtdecode:2.0.1")
     implementation("com.google.android.fhir:engine:1.0.0")
-    implementation("com.google.android.fhir:data-capture:1.2.0")
-    implementation("com.google.android.fhir:knowledge:0.1.0-beta01")
-    implementation("com.google.android.fhir:workflow:0.1.0-beta01")
-    implementation("com.hbb20:ccp:2.6.0")
+    implementation("com.google.android.fhir:data-capture:1.1.0")
 
+    // Import the BoM for the Firebase platform
+//    implementation(platform("com.google.firebase:firebase-bom:33.2.0"))
+
+    // Add the dependencies for the Crashlytics and Analytics libraries
+    // When using the BoM, you don"t specify versions in Firebase library dependencies
+//    implementation("com.google.firebase:firebase-crashlytics")
+//    implementation("com.google.firebase:firebase-analytics")
+
+    //Network
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    // Kotlin DSL
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.6.0")
 
-
-
+    implementation("com.hbb20:ccp:2.6.0")
 
 }
